afa594867c38374edafa3098d4d289bb76c289d2
Shubham Kumar <shubhamkr6758@gmail.com>
2025-06-07 20:36:51 +0530

    Add blog type definitions

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Shubham Kumar",
+  DESCRIPTION: "develoepr",
+  AUTHOR: "Shubham",
 }
 
 // Work Page
@@ -37,10 +37,10 @@ export const LINKS: Links = [
     TEXT: "Home", 
     HREF: "/", 
   },
-  { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
+  // { 
+  //   TEXT: "Work", 
+  //   HREF: "/work", 
+  // },
   { 
     TEXT: "Blog", 
     HREF: "/blog", 
@@ -56,26 +56,32 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "forshubhamkumar@gmail.com",
+    HREF: "mailto:forshubhamkumar@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "shubham-kumr",
+    HREF: "https://github.com/shubham-kumr"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "shubham-kumr",
+    HREF: "https://www.linkedin.com/in/shubham-kumr/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "shubhamkumr_",
+    HREF: "https://twitter.com/shubhamkumr_",
   },
+  {
+    NAME: "ENS",
+    ICON: "icon-ethereum",
+    TEXT: "shubhamkumar.base.eth",
+    HREF: "https://app.ens.domains/shubhamkumar.base.eth",
+  }
 ]
 
